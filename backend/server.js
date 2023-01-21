5bb7098590ca021f8c390373386ede8f0ce45072
Surbhi Soni <surbhisoni@Surbhis-MacBook-Pro.local>
2023-01-21 01:29:57 -0500

    connected with databse

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+app.use(cors());
 
-app.listen(8000, () => {
-  console.log("Server is listening");
+//routes
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
