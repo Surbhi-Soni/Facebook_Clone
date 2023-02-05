fad25e93f2d57ab3e5825ed0b14e098d678a6296
Surbhi Soni <surbhisoni@Surbhis-MacBook-Pro.local>
2023-02-05 14:48:13 -0500

    done with css for login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,38 +1,64 @@
+import "./style.css";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/loginInput";
+import { useState } from "react";
+const loginInfos = {
+  email: "",
+  password: "",
+};
 export default function Login() {
+  const [login, setLogin] = useState(loginInfos);
+  const { email, password } = login;
+  console.log(login);
+  const handleLoginChange = (e) => {};
+
   return (
-    <div clasName="login">
-      <div clasName="login_wrapper">
-        <div clasName="login_wrap">
-          <div clasName="login_1">
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
             <img src="../../icons/facebook.svg" alt="" />
             <span>
               Facebook helps you connect and share with the people in your life.
             </span>
           </div>
-          <div clasName="login_2">
-            <div clasName="login_2_wrap">
-              <Formik>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik enableReinitialize initialValues={{ email, password }}>
                 {(formik) => (
                   <Form>
-                    <input type="text" />
-                    <input type="text" />
-                    <button type="submit">Log In</button>
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Email address or Phone number"
+                      onChange={handleLoginChange}
+                    />
+                    <LoginInput
+                      type="text"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleLoginChange}
+                    />
+                    <button type="submit" className="blue_btn">
+                      Log In
+                    </button>
                   </Form>
                 )}
               </Formik>
-              <Link to="/forgot">Forgot Password?</Link>
+              <Link to="/forgot" className="forgot_password">
+                Forgotten password ?
+              </Link>
               <div className="sign_splitter"></div>
               <button className="blue_btn open_signup">Create Account</button>
             </div>
-            <Link to="/">
+            <Link to="/" className="sign_extra">
               <b>Create a Page </b>
               for a celebrity, brand or business.
             </Link>
           </div>
         </div>
-        <div clasName="register"></div>
+        <div className="register"></div>
       </div>
     </div>
   );
