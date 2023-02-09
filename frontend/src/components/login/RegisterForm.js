c69f2aec9ab45c44e8a6c3abe70c6822cd6c1ee0
Surbhi Soni <surbhisoni@Surbhis-MacBook-Pro.local>
2023-02-09 02:58:11 -0500

    completed login and registration part with responsiveness

---
+++ frontend/src/components/login/RegisterForm.js
@@ -0,0 +1,212 @@
+import { Formik, Form } from "formik";
+import { useState } from "react";
+import RegisterInput from "../inputs/registerInput";
+import * as Yup from "yup";
+const userInfos = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+  bYear: new Date().getFullYear(),
+  bMonth: new Date().getMonth() + 1,
+  bDay: new Date().getDate(),
+  gender: "",
+};
+
+export default function RegisterForm() {
+  const [user, setUser] = useState(userInfos);
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+  const yearTemp = new Date().getFullYear();
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const years = Array.from(new Array(100), (val, index) => yearTemp - index);
+  const months = Array.from(new Array(12), (val, index) => 1 + index);
+  const getDays = () => {
+    return new Date(bYear, bMonth, 0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (val, index) => 1 + index);
+
+  const registerValidation = Yup.object({
+    first_name: Yup.string()
+      .required("What's is your First name?")
+      .min(2, "First name must be 2 and 16 characters")
+      .max(16, "First name must be 2 and 16 characters")
+      .matches(/^[aA-zZ]+$/, "Numbers and special characters are not allowed"),
+    last_name: Yup.string()
+      .required("What's is your Last name?")
+      .min(2, "Last name must be 2 and 16 characters")
+      .max(16, "Last name must be 2 and 16 characters")
+      .matches(/^[aA-zZ]+$/, "Numbers and special characters are not allowed"),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if you ever need to reset your password"
+      )
+      .email("Enter a velid Email address"),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers, letters and punctuation marks(such as | and &)"
+      )
+      .min(6, "Password must be at least 6 characters long")
+      .max(36, "Password can't be more than 36 characters"),
+  });
+
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon"></i>
+          <span>Sign Up</span>
+          <span>It's quick and easy</span>
+        </div>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
+          validationSchema={registerValidation}
+        >
+          {(formik) => (
+            <Form className="register_form">
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="First Name"
+                  name="first_name"
+                  onChange={handleRegisterChange}
+                />
+                <RegisterInput
+                  type="text"
+                  placeholder="Surname"
+                  name="last_name"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="Mobile number or Email address"
+                  name="email"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="password"
+                  placeholder="New password"
+                  name="password"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Date of birth <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <select
+                    name="bDay"
+                    onChange={handleRegisterChange}
+                    value="bDay"
+                  >
+                    {days.map((day, i) => (
+                      <option value={day} key={i}>
+                        {day}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    name="bMonth"
+                    value={bMonth}
+                    onChange={handleRegisterChange}
+                  >
+                    {months.map((month, i) => (
+                      <option value={month} key={i}>
+                        {month}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    name="bYear"
+                    value={bYear}
+                    onChange={handleRegisterChange}
+                  >
+                    {years.map((year, i) => (
+                      <option value={year} key={i}>
+                        {year}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Gender <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <label htmlFor="male">
+                    Male
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="male"
+                      id="male"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="female">
+                    Female
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="female"
+                      id="female"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="Custom">
+                    Custom
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="custom "
+                      id="custom"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="reg_infos">
+                By clicking Sign Up, you agree to our{" "}
+                <span>Terms, Data policy &nbsp;</span>and{" "}
+                <span>Cookie Policy.</span>
+                You may receive SMS notifications from us and can opt out
+                anytime.
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn open_signup">Sign Up</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
